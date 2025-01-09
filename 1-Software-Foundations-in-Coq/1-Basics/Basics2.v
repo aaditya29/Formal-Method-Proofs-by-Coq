2ec825e10b26bb79fa6c3f13115d11ab8dde5f5e
Aditya Mishra <mishraaaditya29@gmail.com>
2025-01-09 12:45:44 +0530

    added negb boolean function

--- 1-Software-Foundations-in-Coq/1-Basics/Basics2.v
+++ 1-Software-Foundations-in-Coq/1-Basics/Basics2.v
@@ -10,3 +10,18 @@ This is analogous to defining a boolean type with two possible values: true and 
 Inductive bool: Type:=
 | true
 | false.
+
+(*Definiing a new function in the boolean for giving output in negation*)
+(** In `Definition negb (b:B) : B :=` the keyword definition is used to define a function or a constant in Coq.
+    `negb` is the name of the function being defined.
+    `b:B` b is the input parameter to the function. `:B` specifies that :b is the type of Boolean. In coq `B` defines type of booleans which can have only two values i.e. true or false
+    `:B` specifies the return type of the function which is also a boolean B*)
+
+(** `match b with ... end` uses a match expression to analyze the value of b and return the appropriate result.
+    `match` keyword is used for pattern matching in Coq. Pattern matching allows us to "case-analyze" the possible values of a type.
+    `b with` specifies that we are matching on the value of b.*)
+Definition negb (b:B) : B :=
+    match b with
+    | true => false
+    | false => true
+    end.
