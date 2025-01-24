112b45d49303f4695038e548353d017a1718ed62
Aditya Mishra <mishraaaditya29@gmail.com>
2025-01-24 10:01:23 +0530

    git commit

--- 1-Software-Foundations-in-Coq/1-Basics/Basics3.v
+++ 1-Software-Foundations-in-Coq/1-Basics/Basics3.v
@@ -12,7 +12,7 @@ Inductive rgb : Type :=
     | green
     | blue.
 
-Inductive colour : True :=
+Inductive colour : Type :=
     | black
     | white
     | primary (p : rgb). (* defining black and white and third colour primary which takes p as argument of rgb type*)
