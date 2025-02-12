1d20255bd06f57c2cd897cfc7a905d6e70e36592
Aditya Mishra <mishraaaditya29@gmail.com>
2025-02-12 17:42:46 +0530

    git commit

--- 1-Software-Foundations-in-Coq/2-Naturals/Naturals2.v
+++ 1-Software-Foundations-in-Coq/2-Naturals/Naturals2.v
@@ -1,1 +1,9 @@
-(* Unary S and O Representation*)
+Module NatPlayground.
+Definition minustwo (n : nat) : nat :=
+    match n with
+    | O => O (* if n is 0 then it returns 0*)
+    | S O => O(* if n is successor of 0 then it returns 0*)
+    | S (S n') => n'(* if n' is successor of successor of n then it returns n'*)
+end.
+
+Compute (minustwo 4).
