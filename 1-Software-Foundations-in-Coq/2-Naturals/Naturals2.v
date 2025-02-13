15e8affb4605ca43f3280555da6dd3b26643d1f1
Aditya Mishra <mishraaaditya29@gmail.com>
2025-02-13 18:25:03 +0530

    added fixpoint

--- 1-Software-Foundations-in-Coq/2-Naturals/Naturals2.v
+++ 1-Software-Foundations-in-Coq/2-Naturals/Naturals2.v
@@ -7,3 +7,31 @@ Definition minustwo (n : nat) : nat :=
 end.
 
 Compute (minustwo 4).
+
+(**Recursive functions are defined using the [FIXPOINT] keyword. It is a venacular command*)
+Fixpoint even(n: nat) : B :=
+    match n with
+    | O => true
+    | S O => false
+    | S (S n') => even n'
+end.
+
+(*Defining odd in the same way*)
+Definition odd(n:nat) : Bool :=
+    negb (even n).
+
+
+(* Adding two numbers*)
+Fixpoint plus (n m : nat) : nat :=
+    match n with
+    | O => m
+    | S n' => S (plus n' m)
+end.
+
+(* Multiplying two numbers*)
+Fixpoint mul (m n: nat) : nat :=
+    match n with
+    | O => O
+    | S n' => plus m (mul m n')
+    end.
+
