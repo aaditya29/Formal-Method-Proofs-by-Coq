2ab42ded27ede05a8acfd69779ad17a201778722
Aditya Mishra <mishraaaditya29@gmail.com>
2025-03-05 09:45:31 +0530

    added proof by intros

--- 1-Software-Foundations-in-Coq/3-Intros/Intros.v
+++ 1-Software-Foundations-in-Coq/3-Intros/Intros.v
@@ -2,3 +2,11 @@ Theorem plus_0_n : forall n: nat, 0+n = n.
 Proof.
 intros n. simpl. reflexivity. Qed.
     
+
+Theorem plus_1_1 : forall n:nat, 1+n = S n.
+Proof.
+    intros n. simpl. reflexivity. Qed.
+
+Theorem mult_0_1: forall n:nat, 0*n=0.
+Proof.
+    intros n. simpl. reflexivity. Qed.
