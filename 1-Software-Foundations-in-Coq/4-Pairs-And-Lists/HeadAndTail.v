2050379a1ed5cf34bee5edc99e5aad4a13289680
Aditya Mishra <mishraaaditya29@gmail.com>
2025-03-18 15:26:43 +0530

    Added HeadAndTail.v

---
+++ 1-Software-Foundations-in-Coq/4-Pairs-And-Lists/HeadAndTail.v
@@ -0,0 +1,4 @@
+(* Defining a list*)
+
+(** We define the list using Inductive keyword.
+In Coq, the Inductive keyword is used to define inductive types. Inductive types are a fundamental concept in Coq, allowing you to define new data types by specifying their constructors. These types can be used to represent various kinds of data structures, such as natural numbers, lists, trees, and more. *)
