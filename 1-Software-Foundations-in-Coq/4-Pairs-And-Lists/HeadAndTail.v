f22c89f32d570354f71554e3b297926ede4ae764
Aditya Mishra <mishraaaditya29@gmail.com>
2025-03-18 15:31:37 +0530

    Enhanced HeadAndTail.v with inductive list definition and explanations

--- 1-Software-Foundations-in-Coq/4-Pairs-And-Lists/HeadAndTail.v
+++ 1-Software-Foundations-in-Coq/4-Pairs-And-Lists/HeadAndTail.v
@@ -2,3 +2,12 @@
 
 (** We define the list using Inductive keyword.
 In Coq, the Inductive keyword is used to define inductive types. Inductive types are a fundamental concept in Coq, allowing you to define new data types by specifying their constructors. These types can be used to represent various kinds of data structures, such as natural numbers, lists, trees, and more. *)
+
+Inductive list (A : Type) : Type :=
+    | nil : list A
+    | const : A -> list A -> list A. (* gere we are adding an element (head) to the list (tail) *)
+ 
+(* In above example nil represents a list which is empty*)
+(* cons h t reperesents a list where 
+- h is the first element and t is the rest of the list*)
+
