58b35c04f45e469290f7a7f67b2e3e48bfbab7f3
Aditya Mishra <mishraaaditya29@gmail.com>
2025-03-18 13:12:23 +0530

    added repeat function to generate a list of repeated elements

--- 1-Software-Foundations-in-Coq/4-Pairs-And-Lists/Lists.v
+++ 1-Software-Foundations-in-Coq/4-Pairs-And-Lists/Lists.v
@@ -9,3 +9,14 @@ Inductive natlist : Type := (* type of the list is the nat*)
 
 Definition mylist := cons 1 (const 2 ( const 3 nil)). (* list of 3 elements*)
 
+
+(* DEFINING A FUNCTION OF LIST*)
+
+(*1. Counting copies of N*)
+(* Here we are making a recursive function name repeat which takes two arguments n and count of nat type. Further the return type of natlist *)
+Fixpoint repeat ( n count : nat) : natlist :=
+  match count with
+  | 0 => nil (* if count is 0 then it will return empty list*)
+  | S count' => n :: (repeat n count') (* if count is some successor of S count' then it will return a list where n is added tot he result of recursively calling repeat with n and count'*)
+  end.
+Compute repeat 42 3.
